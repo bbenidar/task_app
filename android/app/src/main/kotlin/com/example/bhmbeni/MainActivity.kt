abb2f884f72b51049e13bc9afaac259b31fe2d4b
Brahim Benidar <bbenidar@e1r10p11.1337.ma>
2023-04-07 00:42:15 +0000

    task app

---
+++ android/app/src/main/kotlin/com/example/bhmbeni/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bhmbeni
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utils/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class MyColors {
+  static const Color primaryColor = Color(0xffdc4572);
+  static const List<Color> primaryGradientColor = [
+    Color(0xffdc4572),
+    Color(0xffb36a6a),
+  ];
+}
